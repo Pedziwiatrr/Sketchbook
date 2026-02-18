5ad22f525b9f3291f80795b93e5d9241f3190369
Pedziwiatrr <michalozaurus@gmail.com>
2026-02-18 11:01:03 +0100

    fix: update sketches order to match README

--- pencil_sketch_processor.py
+++ pencil_sketch_processor.py
@@ -8,9 +8,9 @@ SKETCHES = [
     "2026_02_18_berserk_pencil_2.jpeg",
     "2026_02_17_jinx_pencil_1.jpeg",
     "2026_02_18_jinx_pencil_3.jpeg",
+    "2026_02_16_jinx_pencil_4.jpeg",
     "2026_02_18_skull_pencil_1.jpeg",
     "2026_02_17_reptile_pencil_1.jpeg",
-    "2026_02_16_jinx_pencil_4.jpeg",
     "2026_02_17_jinx_pencil_2.jpeg",
 ]
 
